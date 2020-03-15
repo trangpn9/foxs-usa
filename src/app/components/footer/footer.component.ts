41c135f24ccc517e139575c677b02e93006fe2f4
trangpn9 <trangpn90@gmail.com>
2020-03-15 21:31:00 +0700

    footer content done

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppSetting } from '../../constants/init-api';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  data:any = '';
+  content_short:any = '';
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.http.get(`${AppSetting.API_SERVICE}wp/v2/posts/26`).subscribe((data) => {
+      this.data = {...data};
+      this.content_short = this.data.content.rendered;      
+    }, (err) => {
+      console.log(err);
+    });
   }
 
 }
